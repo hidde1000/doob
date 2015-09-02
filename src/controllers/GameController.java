8d8b50a850c2096812f78a4c4177cee898eb6633
Jasper van Tilburg <J.F.vantilburg@student.tudelft.nl>
2015-09-02 16:24:19 +0200

    First bouncing ball added

---
+++ src/controllers/GameController.java
@@ -0,0 +1,58 @@
+package controllers;
+
+import java.util.ArrayList;
+
+import Application.Ball;
+import Application.Main;
+import javafx.animation.AnimationTimer;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+
+public class GameController {
+	
+	@FXML
+	private Canvas canvas;
+	private GraphicsContext gc;
+	
+	private ArrayList<Ball> balls;
+	private int ballSpeed = 3;
+
+	@FXML
+	public void initialize() {
+		balls = new ArrayList<Ball>();
+		balls.add(new Ball(100, 500, ballSpeed, 0, 100));
+		gc = canvas.getGraphicsContext2D();
+		startTimer();
+	}
+	
+	public void moveBalls() {
+		for(Ball b : balls) {
+			b.moveHorizontally();
+			if(b.getX() + b.getSize() >= canvas.getWidth()) b.setSpeedX(-ballSpeed);
+			else if(b.getX() <= 0) b.setSpeedX(ballSpeed);
+			b.moveVertically();;
+			b.incrSpeedY(0.5);
+			if(b.getY() + b.getSize() > canvas.getHeight()) {
+				b.setSpeedY(-20);
+			}
+		}
+	}
+	
+	public void paint() {
+		gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+		for(Ball b : balls) {
+			gc.fillOval(b.getX(), b.getY(), b.getSize(), b.getSize());
+		}
+	}
+
+	public void startTimer() {
+		 new AnimationTimer() {
+	            @Override
+	            public void handle(long now) {
+	               moveBalls();
+	               paint();
+	            }
+	        }.start();
+	}
+}
--- src/controllers/MenuController.java
+++ src/controllers/MenuController.java
@@ -3,9 +3,10 @@ package controllers;
 import javafx.fxml.FXML;
 
 public class MenuController {
-
+	
 	@FXML
 	public void playSinglePlayer() {
 		Application.Main.loadScene("/Game.fxml");
 	}
+	
 }
